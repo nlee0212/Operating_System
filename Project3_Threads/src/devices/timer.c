39df30796b53451ff533a6273a42ee5ccdf27fb6
Na Yeon Lee <44570724+nlee0212@users.noreply.github.com>
2020-11-27 21:31:19 +0900

    Update timer.c
    Alarm Clock implementation in timer_sleep()

--- Project3_Threads/src/devices/timer.c
+++ Project3_Threads/src/devices/timer.c
@@ -92,8 +92,13 @@ timer_sleep (int64_t ticks)
   int64_t start = timer_ticks ();
 
   ASSERT (intr_get_level () == INTR_ON);
+  /* Original code */
   while (timer_elapsed (start) < ticks) 
     thread_yield ();
+
+  /* MY CODE */
+  // Alarm Clock implementation
+  thread_sleep(start + ticks);
 }
 
 /* Sleeps for approximately MS milliseconds.  Interrupts must be
