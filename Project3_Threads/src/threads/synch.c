b10f7cc622042b0a12b53779f5dc009af67e8295
Na Yeon Lee <44570724+nlee0212@users.noreply.github.com>
2020-12-02 14:07:24 +0900

    trim

--- Project3_Threads/src/threads/synch.c
+++ Project3_Threads/src/threads/synch.c
@@ -115,7 +115,6 @@ sema_up (struct semaphore *sema)
   old_level = intr_disable ();
   if (!list_empty(&sema->waiters)) 
       thread_unblock(list_entry(list_pop_front(&sema->waiters), struct thread, elem));
-  
     
   sema->value++;
 
@@ -125,8 +124,6 @@ sema_up (struct semaphore *sema)
 #endif
 
   intr_set_level (old_level);
-  
-
 }
 
 static void sema_test_helper (void *sema_);
